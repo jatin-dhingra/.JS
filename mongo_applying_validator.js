dbb51a21962b72f46b7e3d94a123d0f961ec4d6f
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-01-09 00:32:14 +0530

    Create mongo_applying_validator.js

---
+++ mongo_applying_validator.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shopApp')
+    .then(()=>{
+        console.log("connected");
+    })
+    .catch(err=>
+    {
+        console.log("error");
+        console.log(err)
+    })
+
+const productSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required: true,
+        maxlength:15
+    },
+    price:{
+        type:Number,
+        min:0
+    },
+    onsale:{
+        type:Boolean,
+        default:false
+    },
+    category:[String],
+    qty:{
+        online:{
+            type:Number,
+            default:0
+        },
+        instore:{
+            type:Number,
+            default:0
+        }
+    }
+
+
+});
+
+const Product=mongoose.model("Product",productSchema)
+
+// Product.insertMany([
+//         {name:"lana rhodes",price:7,onsale:true,category:['porn star','busty']},
+//         {name:"nicollete",price:500000}
+// ])
+Product.findOneAndUpdate({name:"lana rhodes"},{price:-69},{new:true,runValidators:true})
+    .then(d=>{
+        console.log("works fine")
+        console.log(d)
+    }).catch(err=>{
+        console.log(err)
+    })
